b50642d2c24c95367db7e8738231162180194a03
Joe Kralicky <joekralicky@gmail.com>
2021-11-10 18:07:49 +0000

    Updates

--- scripts/write-kubeconfig.sh
+++ scripts/write-kubeconfig.sh
@@ -3,5 +3,5 @@ if [ $# -ne 1 ]; then
   echo "Usage: get-kubeconfig.sh <stack>"
   exit 1
 fi
-control_plane_ip=$(openstack stack output show test-k3s control_plane_ip -f json | jq -r '.output_value')
-openstack stack output show $1 kubeconfig -f json | jq -r '.output_value | fromjson ."1"' | base64 --decode | sed "s/127.0.0.1/${control_plane_ip}/" | sed "s/default/$1/g" > kubeconfig.yaml
+control_plane_ip=$(openstack stack output show $1 control_plane_ip -f json | jq -r '.output_value')
+openstack stack output show $1 kubeconfig -f json | jq -r '.output_value | fromjson ."1"' | base64 --decode | sed "s/127.0.0.1/${control_plane_ip}/" | sed "s/default/$1/g" > $1.yaml
